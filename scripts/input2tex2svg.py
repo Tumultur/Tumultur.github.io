954ac428d6d4281dd3e77f932e8ef3b3a0a3ad53
Tumultur <josephpmoore@protonmail.com>
2022-11-10 16:16:34 +0000

    Add script for generating formula

---
+++ scripts/input2tex2svg.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+tex_header = r'''\documentclass[varwidth=8in,12pt]{standalone}
+\usepackage{amsmath}
+\usepackage{stix}
+\usepackage{tcolorbox}
+\begin{document}
+\makebox[8in]{$
+'''
+
+tex_tail = r'''
+$}
+\end{document}
+'''
+if sys.argv[2] == 'long':
+    tex = tex_header[:-1]+'$'+sys.argv[1]+'$'+tex_tail[1:]
+elif sys.argv[2] == 'short':
+    tex = tex_header+sys.argv[1]+tex_tail
+with open('output.tex', 'w') as output_tex_file:
+    output_tex_file.write(tex)
+os.system('latexmk output.tex -pdf')
+os.system('pdftocairo -svg output.pdf output.svg')
+os.system('latexmk -C')
+os.system('del output.tex')
